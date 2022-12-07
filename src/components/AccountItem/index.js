79fa0dcc513e967b72abcc7f89a893b6f3d0ff54
Tran Vu Luan <tranvuluan2270@gmail.com>
2022-12-07 19:51:03 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,6 +1,7 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,7 +9,7 @@ const cx = classNames.bind(styles);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/d3010113874413c387665069c7afabbe~c5_720x720.jpeg?x-expires=1670238000&x-signature=32cUVewdiYabgBUDHUYeUsQgLNw%3D"
                 alt="Hoaa"
